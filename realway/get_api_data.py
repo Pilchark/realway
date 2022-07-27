afa0b8590a8098f185e980178e9b0292051140d2
XuQiang <xq_work@outlook.com>
2022-07-27 20:43:51 +0800

    add combination_all_city func

--- realway/get_api_data.py
+++ realway/get_api_data.py
@@ -1,6 +1,6 @@
 # realway/get_api_data.py
 
-from cgitb import text
+import time
 import requests
 import json
 from rich import print
@@ -11,30 +11,57 @@ from datetime import datetime
 # config args
 key = conf["server"]["key"]
 url = conf["server"]["url"]
-cities = conf["info"]["cities"]
 city = conf["city"]
 
-def get_api_data():
-    
-    BJ = city["BeiJing"]["name"]
-    SH = city["ShangHai"]["name"]
-    t_args = {f"start": BJ, "end": SH, "key": key}
+def combination_all_city():
+    for i in iter(city):
+        start = city[i]["name"]
+        for j in iter(city):
+            end = city[j]["name"]
+            if not start == end:
+                start, end, res = get_api_data(start, end)
+                export_json_data(start, end, res)
+            else:
+                pass
 
-    # r = requests.post(f'{url}', params=t_args)
-    r = requests.get("https://binstd.apistd.com/train/line?trainno=G34&key=9U2cUUZhHEqLZx5cx5G2owkd7")
+def get_api_data(start, end):
+    """
+    get all trips from start to end 
+    """
+    t_args = {f"start": start, "end": end, "key": key}
+    r = requests.post(f'{url}', params=t_args)
     text = r.text
-    return text
+    res = json.loads(text)
+    return start, end, res
 
 
-def export_json_data(res = None):
+def export_json_data(start, end, res):
     date_format = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-    json_file = "data/" + date_format + ".json"
+    json_file = "data/" + date_format + "_" + start + "_" + end +".json"
     path = pathlib.Path(__file__).parent / json_file
-    # with path.open(mode="w") as fp:
-    #     json.dump(res, fp)
     with open(path, "w",encoding="utf-8") as f:
         json.dump(res, f, ensure_ascii=False)
 
+def show_api_data():
+    json_file = "data/" + "2022-07-27_16_10_14" + ".json"
+    path = pathlib.Path(__file__).parent / json_file
+    with open(path, "r",encoding="utf-8") as f:
+        data = json.load(f)
+        print(data["result"]["start"])
+        print(data["result"]["end"])
+
+
+def main():
+    for i in iter(city):
+        start = city[i]["name"]
+        for j in iter(city):
+            end = city[j]["name"]
+            if not start == end:
+                print(f"{start} -> {end}")
+                start, end, res = get_api_data(start, end)
+                export_json_data(start, end, res)
+            else:
+                pass
+
 if __name__ == "__main__":
-    res = get_api_data()
-    export_json_data(res)
+    main()
--- tests/test_realway.py
+++ tests/test_realway.py
@@ -1,6 +1,6 @@
 from realway import __version__
 from realway.config import conf
-from pypinyin import pinyin, lazy_pinyin, Style
+from pypinyin import lazy_pinyin
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -10,11 +10,13 @@ def test_config_city_name():
     test if city CN name equal to EN name
     """
     
-    cities = conf["info"]["cities"]
+    city = conf["city"]
+    
+    print(city.keys())
 
-    for i in cities:
-        name = i["name"]
+    for i in city.keys():
+        name = city[i]["name"]
         check_name = "".join(lazy_pinyin(name))
-        pinyin = str(i["pinyin"]).lower()
+        pinyin = str(city[i]["pinyin"]).lower()
         assert check_name == pinyin
 
