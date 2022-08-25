a108cec4d18e3d0ec74c77310d9bdc2e11e48aa4
XuQiang <xq_work@outlook.com>
2022-08-26 00:08:52 +0800

    add get day results

--- realway/app.py
+++ realway/app.py
@@ -7,50 +7,61 @@ base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_dir)
 from realway.config import conf
 
-today = datetime.now().strftime("%Y-%m-%d")
+# today = datetime.now().strftime("%Y-%m-%d")
+
 
 def get_db():
     # get mongo url in docker
-    mongo_url = os.getenv('MONGODB_URL')
+    mongo_url = os.getenv("MONGODB_URL")
     if mongo_url == None:
         # get mongo url in local host
-        mongo_url = conf['mongo']['url']
+        mongo_url = conf["mongo"]["url"]
     client = MongoClient(mongo_url)
-    db = client['realway']
+    db = client["realway"]
     return db
+
 
 app = Flask(__name__)
 
+
 class Config(object):
-    JSON_AS_ASCII = conf['app']['JSON_AS_ASCII']
+    JSON_AS_ASCII = conf["app"]["JSON_AS_ASCII"]
+
 
 app.config.from_object(Config)
 
 # app route
 
+
 @app.route("/")
 def index():
     return "Hello"
 
+
 @app.route("/settings")
 def get_settings():
     return {
-        "json_format": app.config['JSON_AS_ASCII'],
+        "JSON_AS_ASCII": app.config["JSON_AS_ASCII"],
     }
 
-@app.route('/today')
-def get_one():
+
+@app.route("/api/day/<datetime>/")
+def api_day(datetime):
     try:
         db = get_db()
-        res = db[today].find_one({"status":0})
-        output = {
-            "start":res["result"]["start"], 
-            "end":res["result"]["end"], 
-            "nums":len(res["result"]["list"])
+        results = db[datetime].find()
+        l = []
+        for res in results:
+            output = {
+                "start": res["result"]["start"],
+                "end": res["result"]["end"],
+                "lines": len(res["result"]["list"]),
             }
-        return jsonify(output)
+            l.append(output)
+        return jsonify(l)
     except:
         return "fetch data failed!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
