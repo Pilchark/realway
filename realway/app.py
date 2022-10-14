ec524bc007add794c59d5516139dbfbe7b9eef14
XuQiang <xq_work@outlook.com>
2022-10-14 10:39:24 +0800

    disable debug in public server

--- realway/app.py
+++ realway/app.py
@@ -28,10 +28,25 @@ def index():
         end_s = form.end_s.data
         if end_s == "sss":
             return render_template("404.html")
-        return redirect(url_for("search_one_day", date=date, start_s=start_s, end_s=end_s))
+        data = {
+            "date": date,
+            "start_s": start_s,
+            "end_s": end_s,
+        }
+        print(data)
+        # return redirect(
+        #     url_for("test", data)
+        # )
+        return redirect(
+            url_for("api_search_one_day", date=date, start=start_s, end=end_s)
+        )
 
     return render_template("index.html", title="Home", form=form)
 
+@app.route("/test", methods=["GET"])
+def test():
+    args = request.args
+    return args.get("data",None)
 
 @app.route("/api/search", methods=["GET"])
 def api_search_one_day():
@@ -48,47 +63,45 @@ def api_search_one_day():
         start = args.get("start", None)
         end = args.get("end", None)
         if None in (start, end):
-            return {"status": 400,
-                    "msg":  "start or end should not be Empty !"}
+            return {"status": 400, "msg": "start or end should not be Empty !"}
         res = fetcher.get_one_way_data(datetime=datetime, start=start, end=end)
         if res is None:
-            return {"status": 400,
-                    "msg":  "Data not Found !"}
+            return {"status": 400, "msg": "Data not Found !"}
         else:
-            return {"status": 200,
-                    "data": res}
+            return {"status": 200, "data": res}
     else:
         sample_data = os.path.join(base_dir, "sample.json")
         with open(sample_data, "r") as f:
             res = json.load(f)
 
-    res = res["result"]['list']
+    res = res["result"]["list"]
     length = len(res)
     width = 30
     np_data = np.full((length, width), np.NaN, dtype="U20")
-    i,j=0,0
+    i, j = 0, 0
     col = [i for i in res[0].keys()]
     for i_train in res:
         for v in i_train.values():
             np_data[i][j] = v
-            j+=1
-        i+=1
-        j=0
+            j += 1
+        i += 1
+        j = 0
 
     df = pd.DataFrame(np_data, columns=col)
-    df = df.drop(df.columns[[8,9,10,11,12]], axis=1)
+    df = df.drop(df.columns[[8, 9, 10, 11, 12]], axis=1)
     # return df.to_dict()
     df_html = df.to_html(classes="ui celled table", table_id="data")
-    return render_template('table.html', table=df_html, title="test Title")
+    return render_template("table.html", table=df_html, title="test Title")
 
 
 @app.route("/chart_sample")
 def chart_sample():
     return render_template("echart_sample.html")
 
+
 @app.route("/element_sample")
 def element_sample():
-    return render_template("echart_sample.html")
+    return render_template("element_sample.html")
 
 
 if __name__ == "__main__":
